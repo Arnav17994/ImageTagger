ec4d3d752f17d6fd3fe5dac4fb3991ca25de8f48
Arnav17994 <arnav@wayload.co>
2019-04-20 15:01:43 +0530

    Working

--- App.js
+++ App.js
@@ -1,16 +1,18 @@
 import React from 'react';
-import { StyleSheet, Image, View, Button } from 'react-native';
+import { StyleSheet, Image, View, Button, ScrollView, Text, Dimensions, ActivityIndicator } from 'react-native';
 import {ImagePicker, Permissions} from 'expo'
 import axios from 'axios'
-const uuidv4 = require('uuid/v4')
+
+const {width} = Dimensions.get('window')
 
 export default class App extends React.Component {
 
   state = {
     galleryImage: null,
+    upload_id: null, 
+    tags: null,
+    isLoading: false
   }
-
-  tagsArray = []
 
   openGallery = async () => {
     let galleryImage = await ImagePicker.launchImageLibraryAsync({base64: true, quality: 0.15})
@@ -21,23 +23,32 @@ export default class App extends React.Component {
   }
 
   uploadImage() {
-    // let image_base64 = this.state.galleryImage.base64.toString()
-
-    axios(`https://api.imagga.com/v2/uploads`, {
-      method: 'post',
+    this.setState({isLoading: true})
+    const formData = new FormData()
+    formData.append('image_base64', this.state.galleryImage.base64)
+    axios({
+      url: "https://api.imagga.com/v2/uploads",
+      method: 'POST',
       headers: {
-        "Authorization": "Basic YWNjX2IxYmRhMzRiYjM1ZmFiNTo5Njg1NjNiMTMwODViYzAyZDgyZWFmOGU2ZWQyODU3MA==",
+        "Authorization": "Basic YWNjX2IxYmRhMzRiYjM1ZmFiNTo5Njg1NjNiMTMwODViYzAyZDgyZWFmOGU2ZWQyODU3MA=="
       },
-      data: {
-        "image_base64": this.state.galleryImage.base64
-      }
+      data: formData
     })
-    .then(response => console.log(response))
-    // .then(response => response && response.result.tags.map((item, index) => 
-    //   tagsArray.push({key: index, tag: item.tag.en, confidence: item.confidence}))
-    //   )
-    .catch(error => console.log(error))
+    .then(response => this.setState({upload_id: response.data.result.upload_id}))
+    .then(() => this.getTags())
+    .then(() => this.setState({isLoading: false}))
   }
+
+  getTags = () => {
+    axios({
+      url: `https://api.imagga.com/v2/tags?image_upload_id=${this.state.upload_id}`,
+      method: 'GET',
+      headers: {
+        "Authorization": "Basic YWNjX2IxYmRhMzRiYjM1ZmFiNTo5Njg1NjNiMTMwODViYzAyZDgyZWFmOGU2ZWQyODU3MA=="
+      },
+    }).then(response => this.setState({
+      tags: response.data.result.tags.filter(item => item.confidence > 25)}))
+}
 
   async componentDidMount() {
     const { status } = await Permissions.askAsync(Permissions.CAMERA);
@@ -47,16 +58,29 @@ export default class App extends React.Component {
   render() {
     return (
       <View style={styles.container}>
-        <View style={{flex: 3}}>
+      <ScrollView style={{flex: 1}}>
             {
-              this.state.galleryImage && 
-              <Image source={{uri: this.state.galleryImage.uri}} style={{width: 400, height: 400}} />
+              this.state.galleryImage ? 
+              <Image source={{uri: this.state.galleryImage.uri}} style={{width: width, height: 400}} />
+              :
+              <View style={styles.imagePlaceholder}>
+                <Text style={{color: '#a9a9a9', fontSize: 20}}>No Image Selected</Text>
+              </View>
             }
-        </View>
-        <View style={{flex: 1, justifyContent: 'center'}}>
+        <View style={{flex: 1, justifyContent: 'space-between', flexDirection: 'row', padding: 20}}>
         <Button title="Select Image" onPress={() => this.openGallery()} />
         <Button title="Get Tags" onPress={() => this.uploadImage()} />
         </View>
+        <View>
+            {
+              this.state.tags &&
+              this.state.tags.map((item, index) => <View key={index} style={styles.tagsContainer}><Text style={{marginLeft: 10, color: "#fff"}}>{item.tag.en}</Text></View>)
+            }
+        </View>
+        {
+          this.state.isLoading && <ActivityIndicator size="large" color="#865CD6"/>
+        }
+        </ScrollView>
       </View>
     );
   }
@@ -69,4 +93,20 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     marginVertical: 20
   },
+  tagsContainer: {
+    margin: 5, 
+    padding: 5, 
+    height: 30, 
+    borderRadius: 15, 
+    justifyContent: 'center', 
+    backgroundColor: "#865CD6", 
+    alignItems: 'center'
+  },
+  imagePlaceholder: {
+    height: 400, 
+    width: width, 
+    backgroundColor: '#e7e7e7', 
+    alignItems: 'center', 
+    justifyContent: 'center'
+  }
 });
